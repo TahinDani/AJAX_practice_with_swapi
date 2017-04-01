efff60234dd805d8079df99fbf68843d906e88d6
TahinDani <tahin.daniel@gmail.com>
2017-04-01 21:18:10 +0200

    prev/next button works

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 let button = document.querySelector('#button')
 let name = document.querySelector('#name')
 let height = document.querySelector('#height')
@@ -5,45 +6,43 @@ let mass = document.querySelector('#mass')
 let birthYear = document.querySelector('#birth-year')
 let next_button = document.querySelector('#next_button')
 let prev_button = document.querySelector('#prev_button')
-var pageCounter = 1
-
+var nextPage
+var prevPage
 
 function getInfo() {
-    
+    prev_button.disabled = "true"
     axios.get('http://swapi.co/api/people/?page=1').then(function(response){
         updateInfo(response.data.results[0]);  // display the first people of current page
         console.log(response.status); // for testing
-           
+        nextPage = response.data.next;
+        prevPage = response.data.previous;
+        checkPage();
     }).catch(function (error) {
         showErrorMessage();
     });  
 }
 
 function nextPage() {
-    pageCounter ++
-    axios.get('http://swapi.co/api/people', {
-        params: {
-            page: pageCounter
-                }
-    }).then(function(response){
-        checkPage();  // does not work, don't know why
-        updateInfo(response.data.results[0]);  
-        console.log(response.status);
-        
+    
+    axios.get(nextPage).then(function(response){
+        updateInfo(response.data.results[0]);  // display the first people of current page
+        console.log(response.status);  // for testing
+        nextPage = response.data.next;
+        prevPage = response.data.previous;
+        checkPage();
     }).catch(function (error) {
         showErrorMessage();
     });
 }
 
 function prevPage() {
-    pageCounter --
-    axios.get('http://swapi.co/api/people', {
-        params: {
-            page: pageCounter
-                }
-    }).then(function(response){
-        updateInfo(response.data.results[0]);
-        console.log(response.status); // for testing       
+    
+    axios.get(prevPage).then(function(response){
+        updateInfo(response.data.results[0]);  // display the first people of current page
+        console.log(response.status); // for testing
+        nextPage = response.data.next;
+        prevPage = response.data.previous;
+        checkPage();  
     }).catch(function (error) {
         showErrorMessage();
     });
@@ -61,8 +60,10 @@ function showErrorMessage() {
     name.innerText = '!Error!';
 }
 
-function checkPage(){       // does not work, don't know why
-    if (pageCounter > 9){ console.log(pageCounter);}
+function checkPage(){
+    if (prevPage == null) {prev_button.disabled=true;} else {prev_button.disabled=false;};
+    if (nextPage == null) {next_button.disabled=true;} else {next_button.disabled=false;};
+    
 }
 
 button.addEventListener('click', getInfo)
